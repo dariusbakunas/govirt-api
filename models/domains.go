2ef07da6603b15669809a7383429f867b5cb1a11
Darius Bakunas-Milanowski <bakunas@gmail.com>
2018-08-28 20:05:55 -0400

    add domain state and uuid

--- models/domains.go
+++ models/domains.go
@@ -6,11 +6,35 @@ import (
 )
 
 type Domain struct {
+	ID uint
 	Name string
+	UUID string
+	State string
+}
+
+func getDomainStateString(state libvirt.DomainState) string {
+	switch state {
+	case libvirt.DOMAIN_SHUTOFF:
+		return "shut off";
+	case libvirt.DOMAIN_RUNNING:
+		return "running";
+	case libvirt.DOMAIN_PAUSED:
+		return "paused";
+	case libvirt.DOMAIN_BLOCKED:
+		return "blocked";
+	case libvirt.DOMAIN_CRASHED:
+		return "crashed";
+	case libvirt.DOMAIN_PMSUSPENDED:
+		return "suspended";
+	case libvirt.DOMAIN_NOSTATE:
+		fallthrough
+	default:
+		return "unknown";
+	}
 }
 
 func (d Domain) GetAll() ([]Domain, error) {
-	libvirtDoms, err := libvirtConn.ListAllDomains(libvirt.CONNECT_LIST_DOMAINS_ACTIVE)
+	libvirtDoms, err := libvirtConn.ListAllDomains(libvirt.CONNECT_LIST_DOMAINS_ACTIVE | libvirt.CONNECT_LIST_DOMAINS_INACTIVE)
 	if err != nil {
 		log.Fatalf("failed to get domain list: %v", err)
 		return nil, err
@@ -22,12 +46,28 @@ func (d Domain) GetAll() ([]Domain, error) {
 		name, err := domain.GetName()
 
 		if err != nil {
-			log.Fatalf("failed to get domain name: %v", err)
 			return nil, err
 		}
 
+		id, err := domain.GetID()
+
+		if err != nil {
+			return nil, err
+		}
+
+		uuid, err := domain.GetUUIDString()
+
+		if err != nil {
+			return nil, err
+		}
+
+		state, _, err := domain.GetState()
+
 		domains[i] = Domain {
+			ID: id,
 			Name: name,
+			UUID: uuid,
+			State: getDomainStateString(state),
 		}
 	}
 
